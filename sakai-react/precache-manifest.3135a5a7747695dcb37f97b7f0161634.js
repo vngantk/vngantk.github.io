f48ff4a6ca381d8e80e086105ba70fda5d2018ce
Vincent Ngan <vngantk@users.noreply.github.com>
2021-09-24 17:48:18 +0800

    Check in 1

---
+++ sakai-react/precache-manifest.3135a5a7747695dcb37f97b7f0161634.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749099840d76a4845d806696e9e91540",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e8ede3ebab6d4f747b2",
+    "url": "./static/css/2.ecd82bd6.chunk.css"
+  },
+  {
+    "revision": "ad3585453ab98f3d904f",
+    "url": "./static/css/main.feea9521.chunk.css"
+  },
+  {
+    "revision": "9e8ede3ebab6d4f747b2",
+    "url": "./static/js/2.b2961127.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "./static/js/2.b2961127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7587ef536ef1c6ff6af1",
+    "url": "./static/js/3.1649b53e.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "./static/js/3.1649b53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3585453ab98f3d904f",
+    "url": "./static/js/main.80f719d1.chunk.js"
+  },
+  {
+    "revision": "fd26779520c534c0c6bc",
+    "url": "./static/js/runtime-main.2087ade3.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "./static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "./static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "./static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "./static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "./static/media/primeicons.b0f5d02f.eot"
+  }
+]);
